bb03244ef78b0cfdbdd0cf5f8932ce601d2ad43b
joflucki <91982622+joflucki@users.noreply.github.com>
2025-11-27 16:19:45 +0100

    Fixed multilingual dynamic routing

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { LANGUAGES } from './i18n/ui';
+
+const LANGUAGE_KEYS = Object.keys(LANGUAGES) as [keyof typeof LANGUAGES];
 
 const posts = defineCollection({
 	// Load Markdown and MDX files in the `src/content/{locale}/posts/` directory.
-	loader: glob({ base: './src/content/en-us/posts', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
+			locale: z.enum(LANGUAGE_KEYS),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
@@ -18,12 +22,13 @@ const posts = defineCollection({
 
 const projects = defineCollection({
 	// Load Markdown and MDX files in the `src/content/projects/` directory.
-	loader: glob({ base: './src/content/en-us/projects', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
+			locale: z.enum(LANGUAGE_KEYS),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
