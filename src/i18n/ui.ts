0be495c901ad11992ca563ae777650be01a9cedf
joflucki <91982622+joflucki@users.noreply.github.com>
2025-11-28 14:50:53 +0100

    Added animations and accessibility to header

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -13,6 +13,9 @@ export const UI = {
     "nav.projects": "Projects",
     "nav.education": "Education",
     "nav.volunteering": "Volunteering",
+    "nav.en-us-button-label": "Switch to English",
+    "nav.de-ch-button-label": "Switch to German",
+    "nav.fr-ch-button-label": "Switch to French",
   },
   "fr-ch": {
     "nav.home": "Accueil",
@@ -20,6 +23,9 @@ export const UI = {
     "nav.projects": "Projets",
     "nav.education": "Formation",
     "nav.volunteering": "Bénévolat",
+    "nav.en-us-button-label": "Passer à l'anglais",
+    "nav.de-ch-button-label": "Passer à l'allemand",
+    "nav.fr-ch-button-label": "Passer au français",
   },
   "de-ch": {
     "nav.home": "Startseite",
@@ -27,6 +33,9 @@ export const UI = {
     "nav.projects": "Projekte",
     "nav.education": "Ausbildung",
     "nav.volunteering": "Freiwilligenarbeit",
+    "nav.en-us-button-label": "Wechseln zu English",
+    "nav.de-ch-button-label": "Wechseln zu Deutsch",
+    "nav.fr-ch-button-label": "Wechseln zu Französich",
 
   }
 } as const;
